10fd05eb74ba17e2c7922113312ae6d34b33f1cf
Irina <nika_lavrova@internet.ru>
2023-05-09 19:00:24 +0400

    refactor: make code of the 3rd task solution more readable and concise

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,48 +1,33 @@
-const fs = require("fs");
 const path = require("path");
-const { mkdir, readdir, copyFile } = require("fs/promises");
+const { mkdir, readdir, copyFile, unlink } = require("fs/promises");
 
-let folder, folderCopy, files;
+const folder = path.join(__dirname, "files"),
+  folderCopy = path.join(__dirname, "files-copy");
 
-try {
-  (async () => {
-    folderCopy = path.join(__dirname, "files-copy");
-    const createDir = await mkdir(folderCopy, { recursive: true });
-  })();
-} catch (err) {
-  console.error(err.message);
-}
+(async () => {
+  try {
+    const files = await readdir(folder);
 
-try {
-  (async () => {
-    folder = path.join(__dirname, "files");
-    files = await readdir(folder);
+    await mkdir(folderCopy, { recursive: true });
 
     for (const file of files) {
       const pathToFile = path.join(__dirname, "files", file);
       const fileCopy = path.join(folderCopy, file);
+
       copyFile(pathToFile, fileCopy);
     }
-  })();
-  // await copyFile('source.txt', 'destination.txt');
-} catch {
-  console.error("The file could not be copied");
-}
-
-try {
-  (async () => {
     const copiedFiles = await readdir(folderCopy);
-    const files = await readdir(folder);
 
     for (const file of copiedFiles) {
+
       if (!files.includes(file)) {
         const pathToFile = path.join(folderCopy, file);
-        fs.unlink(pathToFile, (err) => {
-          if (err) throw err;
-        });
+
+        await unlink(pathToFile);
       }
     }
-  })();
-} catch (err) {
-  console.error(err.message);
-}
+  } catch (err) {
+    console.error(err.message);
+  }
+})();
+
